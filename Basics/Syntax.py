f5e288f97c5ed0ff0a2a3e13ab2a1981f2cd94fe
Tomi Ilori <tomiilori123@gmail.com>
2020-10-23 10:34:55 +0100

    Syntax

---
+++ Basics/Syntax.py
@@ -0,0 +1,23 @@
+
+##Indentation##
+
+#in python indentation is very important id represents a block of code rather than just making your code look neat 
+
+#we can see that this block of code works however
+if 5>2:
+    print("five is greater than 2")
+
+#This block of code does not because it is not indented
+"""
+if 5>2:
+print("five is greater than 2")
+"""
+
+##Variables##
+
+#In python variables are created when yoiu asssign a value to it
+x = 5
+y = "Hello, World"
+
+#Comments
+# is used to create comments in python
