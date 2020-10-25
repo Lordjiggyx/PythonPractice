2fc5aaa99ded6e6040e879c36696907540a65aea
Tomi Ilori <tomiilori123@gmail.com>
2020-10-25 10:56:42 +0000

    User input

---
+++ Advanced/User_Input.py
@@ -0,0 +1,6 @@
+#User Input
+
+"User input is simple compared to java and javascriipt"
+"You assign a varibale and use the input()methid following the question or prompt  "
+username = input("What is your name ")
+print(username)
